cf269f877c71f5fe35288ea1b37fe4e396ff5482
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2023-03-29 07:57:26 +0400

    downloaded the required font files

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div className="signup">
+      <div> Register</div>
+      <p>Let's get started with your name</p>
+    </div>
+  );
 };
 
 export default Signup;
